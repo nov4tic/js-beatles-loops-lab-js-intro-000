11e63c6037f6b865b5a208aee39e8b87b06095ca
Dev Frias <ddfrias14@gmail.com>
2021-02-24 05:56:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// add solution here
+function theBeatlesPlay(mus, inst) {
+  let arr = [];
+  for (let i = 0; i < 4; i++) {
+    arr.push(mus[i].concat(' plays ', inst[i]));
+  }
+  return arr;
+}
+
+function johnLennonFacts(arr) {
+  let i = 0;
+  let empty = []
+  while (i < arr.length) {
+    empty.push(arr[i] + '!!!')
+    i++
+  }
+  return empty;
+}
+function iLoveTheBeatles(num) {
+  let emptyA = []
+  do {
+    emptyA.push('I love the Beatles!')
+    num ++
+  } while (num < 15);
+  return emptyA;
+  }
