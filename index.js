d46e0c37a60c23cdaf4b7c04be025524aebcc3b5
Dev Frias <ddfrias14@gmail.com>
2021-02-24 05:01:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// add solution here
+function theBeatlesPlay(mus, inst) {
+  let arr = [];
+  for (let i = 0; i < 4; i++) {
+    arr.push(mus[i].concat(' plays ', inst[i]));
+  }
+  return arr;
+}
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts) {
+  let newArr = []
+  for (let i = 0; i < 4; i++) {
+    newArr.push( facts[i] + '!!!');
+  }
+  return newArr;
+}
